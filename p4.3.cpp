a258965f36cae04864859fccf74b7a890241d19d
jayrana2804 <ranaparesh1754@gmail.com>
2026-09-14 17:33:57 +0530

    Refactor student record management system

--- p4.3.cpp
+++ p4.3.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 int main()
 {
+    int enrollment_number,mobile_number,semester;
+        char student_name[23];
+        char branch[20];
+    int total_marks;
+          float average_marks,percentage;
     short int choice;
     cout<<"****************************************************"<<endl;
     cout<<"STUDENT RECORD MANAGMENT SYSTEM"<<endl;
@@ -17,11 +22,7 @@ int main()
     switch(choice)
     {
     case 1:
-        int enrollment_number,mobile_number,semester;
-        char student_name[23];
-        char branch[20];
-        cout<<"Register New Student";
-        cout<<endl;
+        cout<<"Register New Student"<<endl;
         cout<<"----------------------------------------------------"<<endl;
         cout<<"STUDENT INFORMATION"<<endl;
         cout<<"----------------------------------------------------"<<endl;
@@ -43,16 +44,14 @@ int main()
     case 2:
         cout<<"Display Student Record";
         cout<<endl;
-        cout<<"Enrollment Number"<<setw(9)<<":"<<"26TCEYID"<<endl;
-        cout<<"Student Name"<<setw(14)<<":"<<"Jay rana"<<endl;
-        cout<<"Branch"<<setw(20)<<":"<<"CE"<<endl;
-        cout<<"Semester"<<setw(18)<<":"<<"1st"<<endl;
-        cout<<"Mobile number"<<setw(13)<<":"<<"9999999999"<<endl;
+        cout<<"Enrollment Number"<<setw(9)<<":"<<enrollment_number<<endl;
+        cout<<"Student Name"<<setw(14)<<":"<<name<<endl;
+        cout<<"Branch"<<setw(20)<<":"<<branch<<endl;
+        cout<<"Semester"<<setw(18)<<":"<<semester<<endl;
+        cout<<"Mobile number"<<setw(13)<<":"<<mobile_number<<endl;
         cout<<endl;
         break;
     case 3:
-          int total_marks;
-          float average_marks,percentage;
         cout<<"Enter Student Marks";
         cout<<endl;
         cout<<"Total marks:";
@@ -89,4 +88,5 @@ int main()
     goto M;
  }
 }
+
 
