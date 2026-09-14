eee692302253a5dbce1fafde57da5a8850d03d09
jayrana2804 <ranaparesh1754@gmail.com>
2026-09-14 16:53:17 +0530

    Fix spelling of 'Jay Rana' in output
    Corrected the spelling of 'Jay Rana' to 'Jay Ranaa' in output.

--- new_set.cpp
+++ new_set.cpp
@@ -3,7 +3,7 @@
 using namespace std;
     int main()
     {
-                cout<<setw(50)<<"Jay Rana"<<";"<<endl;
+                cout<<setw(50)<<"Jay Ranaa"<<";"<<endl;
                 cout<<setw(50)<<"Surat"<<";"<<endl;
                 cout<<setw(50)<<"Jay"<<";";
      return 0;
